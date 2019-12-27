ced12e9543b517487cb7a663fe2ac9e824559379
dfinnis <dfinnis@student.42.fr>
2019-12-27 12:21:06 +0100

    feature/solve two

--- computor.py
+++ computor.py
@@ -41,22 +41,40 @@ def find_degree(zero, one, two):
 
 def solve_zero(zero):
     if zero == 0:
-        print("The solution is: all real numbers")
+        print("The solution is:\nAll real numbers")
     else:
         print("Equation unsolvable")
 
 def solve_one(zero, one):
-    print("The solution is: ", -zero/one)
+    print("The solution is:\n{}".format(-zero/one))
 
-def solve_two(equation):
-    solution = 2#######
-    discriminant = 42########
-    print(solution)
+def square_root(n):
+    mn = 0
+    mx = n if n >= 1 else 1
+    while mx - mn > 0.000000001:
+        mid = (mn + mx) / 2
+        if mid * mid < n:
+            mn = mid
+        else:
+            mx = mid
+    return mx
+
+def solve_two(zero, one, two):
+    discriminant = one*one - 4*two*zero
+    solution = -one / (2 * two)
+    if discriminant < 0:
+        sqrt = square_root(-discriminant) / (2 * two)
+        print("Discriminant is strictly negative, the two solutions are:")
+        print("{} + i * {}\n{} - i * {}".format(solution, sqrt, solution, sqrt))
+    elif discriminant == 0:
+        print("Discriminant = 0, one double solution:", solution)
+    else:
+        sqrt = square_root(discriminant) / (2 * two)
+        print("Discriminant is strictly positive, the two solutions are:")
+        print("{}\n{}".format(solution + sqrt, solution - sqrt))
 
 def solve(equation):
-    zero = 0
-    one = 0
-    two = 0
+    zero, one, two = 0, 0, 0
     equation = equation.replace(" ", "").replace("-", "+-").split("=")
     parts = [equation[i].split("+") for i in range(len(equation))]
     if len(parts) != 2:
@@ -72,17 +90,17 @@ def solve(equation):
             one -= one_side
             two -= two_side
 
-        print("side:")
-        print(side)
-        print("-------------")
-        print(zero)
-        print(one)
-        print(two)
-        print("-------------")
-        print(zero_side)
-        print(one_side)
-        print(two_side)
-        print("-------------")
+        # print("side: {}".format(index))
+        # print(side)
+        # print("-------------")
+        # print(zero)
+        # print(one)
+        # print(two)
+        # print("-------------")
+        # print(zero_side)
+        # print(one_side)
+        # print(two_side)
+        # print("-------------")
 
     reduced = "{} + {} * X^1 + {} * X^2 = 0".format(zero, one, two)
     degree = find_degree(zero, one, two)
@@ -93,24 +111,7 @@ def solve(equation):
     elif degree == 1:
         solve_one(zero, one)
     else:
-        solve_two(reduced)
-
-
-# def find_discriminant(equation):
-# 	discriminant = 1
-# 	return discriminant
-
-
-# def display(reduced, degree, solution, discriminant):
-#     print("Reduced form: " + reduced)
-#     print("Polynomial degree: " + str(degree))
-
-#     print("The solution is:")
-    
-    # print("Discriminant is strictly positive, the two solutions are:")
-    # print(str(solution))
-
-    # print("The polynomial degree is stricly greater than 2, I can't solve.")
+        solve_two(zero, one, two)
 
 def main():
     try:
